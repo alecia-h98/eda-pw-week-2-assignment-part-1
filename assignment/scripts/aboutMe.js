e480ba26f5aa52cecb844f60ce894bc572465bd4
Alecia Hagen <alecia.h98@gmail.com>
2024-11-17 03:43:08 -0600

    updates to test

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,10 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name *
 let firstName = 'Alecia';
+console.log( 'firstName' );
 // 2 - Create a second variable called `lastName` and assign it the value of your last name *
 let lastName = 'Hagen';
+console.log( 'lastName' )
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName` *
@@ -46,13 +48,14 @@ if (adventurous === true){
   console.log( 'Adventures are great!' );
 }
 else{
-  console.log( 'How about we stay home>' )
+  console.log( 'How about we stay home>' );
 }
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
+let diceRoll = 'Try again later';
 
 // 14 - Create a variable called `petStatus`. 
 //      Write a conditional that covers the following:
@@ -62,8 +65,16 @@ else{
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
-
-
+let petStatus = 
+if ( pets < allowedPets ){
+  console.log( 'I can have more pets' );
+}
+if ( pets === allowedPets ){
+  console.log( 'I have enough pets' );
+}
+if ( pets > allowedPets ){
+  console.log( 'Oh no, I have too many pets!' )
+}
 // STRETCH GOALS:
 
 // 15 - Make a variable called `mostPets` and a conditional that
